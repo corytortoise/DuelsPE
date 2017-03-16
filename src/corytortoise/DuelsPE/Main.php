dedf78208e863390b2a44ccc898c9f8a74fa55f0
aki senpai | oshiire <endercreeper4ut@gmail.com>
2017-03-16 11:02:04 -0400

    Update Main.php

--- src/corytortoise/DuelsPE/Main.php
+++ src/corytortoise/DuelsPE/Main.php
@@ -57,7 +57,7 @@
       if(!is_dir($this->getDataFolder())) {
         mkdir($this->getDataFolder());
       }
-	    $this->data = new Config($this->getDataFolder() . "data.yml", Config::YAML,array("arenas" => array(), "signs" => array()));
+	    $this->data = new Config($this->getDataFolder() . "data.yml", Config::YAML,array("arenas" => array(), "signs" => array()))->getAll();
       $this->config = $this->getConfig();
       $this->saveResource("messages.yml");
       $this->messages = new Config($this->getDataFolder() . "messages.yml");
